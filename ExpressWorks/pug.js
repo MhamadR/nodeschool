c91207cfe73d6cf3f8aeb6d9bd9f92f44c23d660
MhamadR <mhamadraslan@gmail.com>
2023-12-26 22:11:37 +0200

    feat: Add solution for PUG challenge
    Added index.pug and pug.js with the solution for the third challenge in expressworks

---
+++ ExpressWorks/pug.js
@@ -0,0 +1,17 @@
+const { setEngine } = require("crypto");
+const express = require("express");
+const path = require("path");
+const { allowedNodeEnvironmentFlags } = require("process");
+
+const app = express();
+const port = process.argv[2];
+const filePath1 = process.argv[3];
+const filePath2 = path.join(__dirname, "templates");
+
+app.set("view engine", "pug");
+app.set("views", filePath1 || filePath2);
+
+app.get("/home", (_, res) => {
+  res.render("index", { date: new Date().toDateString() });
+});
+app.listen(port, () => console.log("Server is running on port 3000"));
