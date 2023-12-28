495de73c6c4ceefed49084d848a8e858dfc357c8
MhamadR <mhamadraslan@gmail.com>
2023-12-29 00:17:42 +0200

    feat: Add solution for What's in Query challenge
    Added whats-in-query.js with the solution for the seventh challenge in expressworks

---
+++ ExpressWorks/whats-in-query.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+const port = process.argv[2];
+
+app.get("/search", (req, res) => {
+  res.send(req.query);
+});
+app.listen(port, () => console.log(`Server is running on port ${port}`));
